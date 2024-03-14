46d8356dc43da226791e81c5eda5d47dedba696e
Jingda Li <jl404@st-andrews.ac.uk>
2024-03-14 21:15:41 +0000

    init Student class

--- src/main/java/com/project/allocation/model/Student.java
+++ src/main/java/com/project/allocation/model/Student.java
@@ -3,8 +3,31 @@ package com.project.allocation.model;
 import java.util.Set;
 
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 
-public class Student extends User{
+public class Student extends User {
     @ManyToMany(mappedBy = "interestedUsers")
     private Set<Project> interestProject;
+    @ManyToOne
+    private Project assignedProject;
+
+    public Student() {
+        super();
+    }
+
+    public Set<Project> getInterestProject() {
+        return interestProject;
+    }
+
+    public void setInterestProject(Set<Project> interestProject) {
+        this.interestProject = interestProject;
+    }
+
+    public Project getAssignedProject() {
+        return assignedProject;
+    }
+
+    public void setAssignedProject(Project assignedProject) {
+        this.assignedProject = assignedProject;
+    }
 }
---
+++ src/main/java/com/project/allocation/service/StudentService.java
@@ -0,0 +1,4 @@
+package com.project.allocation.service;
+
+public class StudentService {
+}
