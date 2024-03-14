2292ff6052ec63d4e9b398e6acb61ab3b0e5d952
Chen Chen <cc411@st-andrews.ac.uk>
2024-03-14 00:00:16 +0000

    user class

--- src/main/java/com/project/allocation/model/User.java
+++ src/main/java/com/project/allocation/model/User.java
@@ -1,11 +1,62 @@
 package com.project.allocation.model;
 
-public class User {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
     private String password;
-    private String role; // "STUDENT" or "STAFF"
+    private String firstname;
+    private String lastname;
+    private Role role; // "STUDENT" or "STAFF"
+
+    public User(String username, String password, String firstname, String lastname) {
+        this.username=username;
+        this.password=password;
+        this.firstname=firstname;
+        this.lastname=lastname;
+    }
+
+    public User() {
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+ 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstname;
+    }
+
+    public String getLastName() {
+        return lastname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    // Role enum for better type safety
+    public enum Role {
+        STUDENT, STAFF
+    }
 
 }
